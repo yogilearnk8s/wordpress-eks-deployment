ee6d46b3cc8da4628314df552624021057726de3
Shivayogi M Kamat <shivayogi.mkamat@global.ntt>
2024-04-23 13:00:45 +0530

    added provider kubernetes block

--- main.tf
+++ main.tf
@@ -2,6 +2,13 @@ provider "aws" {
   region     = "ap-south-1"
 }
 
+
+provider "kubernetes" {
+  host                   = module.eks_cluster_creation.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster_creation.cluster_certificate_authority_data)
+  token = data.aws_eks_cluster_auth.default.token
+}
+
 terraform {
   backend "s3" {
     bucket = "yogi-tf"
