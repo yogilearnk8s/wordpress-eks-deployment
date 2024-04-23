3cd1ea39872e504cbb89a1cc0b37c0ef2eb6e66f
Shivayogi M Kamat <shivayogi.mkamat@global.ntt>
2024-04-23 12:54:01 +0530

    fixed error - removed module eks_cluster_creation from outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,11 @@
 //  value       = module.eks_nodegroup_role
 //}
 
-output "eks_cluster_creation" {
-  description = "Subnets created with CIDR blocks"
-  value = module.eks_cluster_creation
+//output "eks_cluster_creation" {
+//  description = "Subnets created with CIDR blocks"
+//  value = module.eks_cluster_creation
 
-}
+//}
 
 //output "nodegroup_creation" {
 //  description = "internet gateway arn"
